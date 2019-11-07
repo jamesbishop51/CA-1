01f6b624a4375b2cf358be13483be1f388773406
James Bishop <S00187350@mail.itsligo.ie>
2019-11-07 11:31:00 +0000

    added student Properties

--- CA 1/Student.cs
+++ CA 1/Student.cs
@@ -8,8 +8,18 @@ namespace CA_1
 {
     class Student
     {
+        public string Name { get; set; }
 
+        public string DOB { get; set; }
 
+        List<Subject> Subject = new List<Subject>();
+
+        public Student (string name, string dob)
+        {
+            Name = name;
+            DOB = dob;
+
+        }
 
     }
 }
